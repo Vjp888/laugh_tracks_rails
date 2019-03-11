d342f85784ffd408d905ab6e39ecfff17d851b5b
vjp888 <rellik888@gmail.com>
2019-03-11 16:06:19 -0600

    Adds basic view for showing a list of comedians

--- spec/features/user_can_view_comedian_index_spec.rb
+++ spec/features/user_can_view_comedian_index_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'User visits the comedians show page' do
   it 'Shows a list of comdians' do
-    com_1 = Comedian.create(name: "steve", city: "town town", age: 54)
-    com_2 = Comedian.create(name: "andrew", city: "town wown", age: 43)
-    com_3 = Comedian.create(name: "harvey", city: "town pown", age: 23)
+    com_1 = Comedian.create(name: "steve", city: "town town", age: 54, thumbnail: "steve.jpg")
+    com_2 = Comedian.create(name: "andrew", city: "town wown", age: 43, thumbnail: "steve.jpg")
+    com_3 = Comedian.create(name: "harvey", city: "town pown", age: 23, thumbnail: "steve.jpg")
 
     visit comedians_path
 
-    within "comedian-#{com_1.id}" do
+    within ".comedian-#{com_1.id}" do
       expect(page).to have_content("Name: steve")
       expect(page).to have_content("Age: 54")
       expect(page).to have_content("City: town town")
     end
 
-    within "comedian-#{com_2.id}" do
+    within ".comedian-#{com_2.id}" do
       expect(page).to have_content("Name: andrew")
       expect(page).to have_content("Age: 43")
       expect(page).to have_content("City: town wown")
     end
 
-    within "comedian-#{com_3.id}" do
+    within ".comedian-#{com_3.id}" do
       expect(page).to have_content("Name: harvey")
       expect(page).to have_content("Age: 23")
       expect(page).to have_content("City: town pown")
